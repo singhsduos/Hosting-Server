5a47105c81d753daa562d5724da91272df3306dc
Neelesh Singh <Neelesh.Singh@sparxitsolutions.com>
2025-01-19 00:40:45 +0530

    update - environement

--- helpers/logger.js
+++ helpers/logger.js
@@ -16,7 +16,7 @@ const colors = {
 winston.addColors(colors);
 module.exports = winston.createLogger({
   format: winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     winston.format.colorize({ all: true }),
     winston.format.errors({ stack: true }),
     winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
